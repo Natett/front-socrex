86b85fcb823b7c6a50baa7a2adc499b373210d9a
Eduardo Cortejoso <eduardo@socrex.com>
2014-09-23 16:27:30 -0400

    First commit

---
+++ app/js/index.js
@@ -0,0 +1,32 @@
+function ctrl($scope){
+  $scope.rows = [['Listing1','www.google.com'],
+                ['Listing2','www.reddit.com'],
+                ['Listing3','www.espn.com',]];
+  $scope.temp = false;
+  
+  $scope.addRow = function(){
+    $scope.temp = false;
+    $scope.addName="";
+  };
+  
+  $scope.deleteRow = function(row){
+    $scope.rows.splice($scope.rows.indexOf(row),1);
+  };
+  
+  $scope.plural = function (tab){
+    return tab.length > 1 ? 's': ''; 
+  };
+  
+  $scope.addTemp = function(){
+    if($scope.temp) $scope.rows.pop(); 
+    else if($scope.addName) $scope.temp = true;
+    
+    if($scope.addName) $scope.rows.push($scope.addName);
+    else $scope.temp = false;
+  };
+  
+  $scope.isTemp = function(i){
+    return i==$scope.rows.length-1 && $scope.temp;
+  };
+  
+}
