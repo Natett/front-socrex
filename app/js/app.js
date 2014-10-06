9eb8ad6e44f1c9375373b008d3eb632d3833b72e
Eduardo Cortejoso <eduardo@socrex.com>
2014-10-06 12:48:42 -0400

    Ironed out views into 2 different partials, temp hardcoded values

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,8 @@ angular.module('socrex', [
   'socrex.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'listCtrl'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'listCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,68 @@
-'use strict';
+var socrexControllers = angular.module('socrex.controllers', []);
 
-/* Controllers */
+socrexControllers.controller('listCtrl', ['$scope', 
+function($scope) {
+    $scope.rows = [['Listing1','www.google.com'],
+                   ['Listing2','www.reddit.com'],
+                   ['Listing3','www.espn.com',]];
+    $scope.listing = ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD']
+    $scope.temp = false;
 
-angular.module('socrex.controllers', []).
-  controller('MyCtrl1', [function() {
+    $scope.addRow = function(){
+    $scope.temp = false;
+    $scope.addName="";
+    };
 
-  }])
-  .controller('MyCtrl2', [function() {
+    $scope.deleteRow = function(row){
+    $scope.rows.splice($scope.rows.indexOf(row),1);
+    };
 
-  }]);
+    $scope.plural = function (tab){
+    return tab.length > 1 ? 's': ''; 
+    };
+
+    $scope.addTemp = function(){
+    if($scope.temp) $scope.rows.pop(); 
+    else if($scope.addName) $scope.temp = true;
+
+    if($scope.addName) $scope.rows.push($scope.addName);
+    else $scope.temp = false;
+    };
+
+    $scope.isTemp = function(i){
+    return i==$scope.rows.length-1 && $scope.temp;
+    };
+    }]);
+
+socrexControllers.controller('listCtrl2', ['$scope', 
+function($scope) {
+    $scope.rows = [['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD'],
+                   ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD'],
+                   ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD']];
+    $scope.temp = false;
+
+    $scope.addRow = function(){
+    $scope.temp = false;
+    $scope.addName="";
+    };
+
+    $scope.deleteRow = function(row){
+    $scope.rows.splice($scope.rows.indexOf(row),1);
+    };
+
+    $scope.plural = function (tab){
+    return tab.length > 1 ? 's': ''; 
+    };
+
+    $scope.addTemp = function(){
+    if($scope.temp) $scope.rows.pop(); 
+    else if($scope.addName) $scope.temp = true;
+
+    if($scope.addName) $scope.rows.push($scope.addName);
+    else $scope.temp = false;
+    };
+
+    $scope.isTemp = function(i){
+    return i==$scope.rows.length-1 && $scope.temp;
+    };
+    }]);
