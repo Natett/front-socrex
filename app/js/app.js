8988c311a33380d00bd36143919c14ae0bfcfffe
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2014-10-07 14:11:29 +0000

    listings call from api and show in the views

--- app/js/app.js
+++ app/js/app.js
@@ -12,6 +12,6 @@ angular.module('socrex', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'listCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'listCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view2'});
 }]);
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,68 +1,102 @@
 var socrexControllers = angular.module('socrex.controllers', []);
 
-socrexControllers.controller('listCtrl', ['$scope', 
-function($scope) {
-    $scope.rows = [['Listing1','www.google.com'],
-                   ['Listing2','www.reddit.com'],
-                   ['Listing3','www.espn.com',]];
-    $scope.listing = ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD']
-    $scope.temp = false;
+socrexControllers.controller('listCtrl', ['$scope' , '$http', '$rootScope',
+    function($scope,$http,$rootScope) {
+        
+    }
+]);
 
-    $scope.addRow = function(){
-    $scope.temp = false;
-    $scope.addName="";
-    };
+socrexControllers.controller('listCtrl2', ['$scope' , '$http', '$location', '$rootScope',
+    function($scope,$http,$location, $rootScope) {
+        $scope.rows = [];
+        
+        $scope.rows2 = [];
+        
+        $rootScope.selectedListing = {};
+                       
+        $scope.temp = false;
+    
+        $scope.addRow = function(){
+            $scope.temp = false;
+            $scope.addName="";
+        };
+    
+        $scope.deleteRow = function(row){
+            $scope.rows.splice($scope.rows.indexOf(row),1);
+        };
+    
+        $scope.plural = function (tab){
+            return tab.length > 1 ? 's': ''; 
+        };
+    
+        $scope.addTemp = function(){
+            if($scope.temp) $scope.rows.pop(); 
+            else if($scope.addName) $scope.temp = true;
+        
+            if($scope.addName) $scope.rows.push($scope.addName);
+            else $scope.temp = false;
+        };
+        
+        $scope.isTemp = function(i){
+            return i==$scope.rows.length-1 && $scope.temp;
+        };
+        
+        $scope.getDetailedListing = function(listingId) {
+            // dummy filters
+    		//var listingId = '542c3f86b43c2c00029a8211';
+    		    
+            var responsePromise = $http({
+    		    //url: 'http://127.0.0.1:5000/listings/filter', 
+                url: 'http://byopapp-api-stage.herokuapp.com/listings/' + listingId,
+                method: 'GET',
+    		    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
+    
+            responsePromise.success(function(data, status, headers, config) {
+                //$scope.rows2 = data.Data;
+                $rootScope.selectedListing = data.Data;
+                //$rootScope.selectedListing['price'] = 3000;
+                $scope.redirecToListingDetail();
+            });
+                
+            responsePromise.error(function(data, status, headers, config) {
+                alert("AJAX failed!");
+            });
+        }
+        
+        $scope.redirecToListingDetail = function(){
+            $location.path( "/view1", false );
+        }
+        
+        $scope.redirecToListingList = function(){
+            $location.path( "/view2", false );
+        }
+        
+        $scope.filterListings = function(item, event) {
+		    // dummy filters
+            // must be bery carefull with the filters value structure, it has to start with single couotes, and de inner quotes be double
+            // otherwise there would be an error in python decoding  
+		    var filters = {'filters':'{"bedroom":2}'};
+		    
+            var responsePromise = $http({
+		        //url: 'http://127.0.0.1:5000/listings/filter', 
+                url: 'http://byopapp-api-stage.herokuapp.com/listings/filter',
+                method: 'POST',
+		        data: $.param(filters),
+		        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            });
 
-    $scope.deleteRow = function(row){
-    $scope.rows.splice($scope.rows.indexOf(row),1);
-    };
-
-    $scope.plural = function (tab){
-    return tab.length > 1 ? 's': ''; 
-    };
-
-    $scope.addTemp = function(){
-    if($scope.temp) $scope.rows.pop(); 
-    else if($scope.addName) $scope.temp = true;
-
-    if($scope.addName) $scope.rows.push($scope.addName);
-    else $scope.temp = false;
-    };
-
-    $scope.isTemp = function(i){
-    return i==$scope.rows.length-1 && $scope.temp;
-    };
-    }]);
-
-socrexControllers.controller('listCtrl2', ['$scope', 
-function($scope) {
-    $scope.rows = [['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD'],
-                   ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD'],
-                   ['*GREAT STUDIO* HARDWOOD FLOORS, CLOSET SPACE, H/HW INCLUDED *1/1/15* (Beacon Hill)','http://boston.craigslist.org/gbs/fee/4699511345.html', '$1500', 'http://images.craigslist.org/00U0U_kPv2pFmpzw0_600x450.jpg', '1BR', '1BD']];
-    $scope.temp = false;
-
-    $scope.addRow = function(){
-    $scope.temp = false;
-    $scope.addName="";
-    };
-
-    $scope.deleteRow = function(row){
-    $scope.rows.splice($scope.rows.indexOf(row),1);
-    };
-
-    $scope.plural = function (tab){
-    return tab.length > 1 ? 's': ''; 
-    };
-
-    $scope.addTemp = function(){
-    if($scope.temp) $scope.rows.pop(); 
-    else if($scope.addName) $scope.temp = true;
-
-    if($scope.addName) $scope.rows.push($scope.addName);
-    else $scope.temp = false;
-    };
-
-    $scope.isTemp = function(i){
-    return i==$scope.rows.length-1 && $scope.temp;
-    };
-    }]);
+            responsePromise.success(function(data, status, headers, config) {
+                $scope.rows2 = data.Data.Listings;
+                console.log($scope.rows2);
+            });
+            
+            responsePromise.error(function(data, status, headers, config) {
+                alert("AJAX failed!");
+            });
+        }
+        
+        // Do the first call
+        $scope.filterListings();
+    }
+]);
