d8102a5e3862b533cf044c0f1f012314bc5e268c
jhonjairoroa877 <jhonjairoroa87@hotmail.com>
2014-10-19 22:16:58 +0000

    bxslider test7

--- web/js/directives.js
+++ web/js/directives.js
@@ -43,68 +43,7 @@ angular.module('socrex.directives', []).
                    '</li>',
       link: linkFn
     }
-  }).directive( 'bxslider2', function() {
-    var linkFn;
-    linkFn = function( scope, element, attrs ) {
-      var slider = element.bxSlider({adaptiveHeight: false, responsive:false});
-      
-      attrs.$observe('bxslider2', function(val) {
-        bxslider2.html('');
-        var directives = $parse(val)(scope);
-        angular.forEach(directives, function(directive) {
-          element.append($compile(directive)(scope));
-        });
-      });
-      
-      scope.$on('reload-slider', function() {
-                slider.reloadSlider();
-            });
-    }
-  
-    return {
-      restrict: 'A',
-      //template: '<li><img src="http://images.craigslist.org/00V0V_7KrsygPW5pf_600x450.jpg" /></li>',
-      template:   '<li ng-repeat="picture in pictures">' +
-                     '<img ng-src="{{picture}}" alt="" />' +
-                   '</li>',
-      link: linkFn
-    }
-  }).directive('startslider',function() {
-    return {
-       restrict: 'A',
-       replace: true,
-       template: '<ul class="bxslider">' +
-                   '<li ng-repeat="picture in pictures">' +
-                     '<img ng-src="{{picture}}" alt="" />' +
-                   '</li>' +
-                  '</ul>',
-       link: function(scope, elm, attrs) {
-          elm.ready(function() {    
-               $("." + $(elm[0]).attr('class')).bxSlider({
-                    mode: 'fade',
-                    autoControls: true,
-                    slideWidth: 360,
-                    slideHeight:600
-            });
-
-          });
-      }
-    };
-}).directive('dynamic2', function ($compile, $parse) {
-  return {
-    restrict: 'A',
-    replace: true,
-    link: function (scope, element, attr) {
-      attr.$observe('dynamic2', function(val) {
-        element.html('');
-        var directives = $parse(val)(scope);
-        angular.forEach(directives, function(directive) {
-          element.append($compile(directive)(scope));
-        });
-      });
-    }
-  };
-}).directive('dynamic3', function ($compile, $parse) {
+  }).directive('dynamic3', function ($compile, $parse) {
   return {
     restrict: 'A',
     replace: true,
@@ -119,20 +58,6 @@ angular.module('socrex.directives', []).
           //slider.reloadSlider();
         });
         slider.reloadSlider();
-      });
-    }
-  };
-}).directive('dynamic', function ($compile, $parse) {
-  return {
-    restrict: 'A',
-    replace: true,
-    link: function (scope, element, attr) {
-      attr.$observe('dynamic', function(val) {
-        element.html('');
-        var directives = $parse(val)(scope);
-        angular.forEach(directives, function(directive) {
-          element.append($compile(directive)(scope));
-        });
       });
     }
   };
