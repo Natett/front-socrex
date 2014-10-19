85666d7d1f729031b3c1baa603bc998381e2a892
jhonjairoroa877 <jhonjairoroa87@hotmail.com>
2014-10-19 21:11:17 +0000

    bxslider test7

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -3,7 +3,7 @@ var socrexControllers = angular.module('socrex.controllers', []);
 socrexControllers.controller('listCtrl', ['$scope' , '$http', '$location', '$rootScope', '$routeParams' ,
     function($scope,$http, $location,$rootScope, $routeParams) {
         $scope.pictures = ["http://images.craigslist.org/00V0V_7KrsygPW5pf_600x450.jpg", "http://images.craigslist.org/00G0G_3yEtFrm5hOJ_600x450.jpg"];
-        $scope.directives = ['<one/>', '<two/>'];
+        $scope.directives = ['<div>Directive one</div>', '<div>Directive two</div>'];
         $scope.add = function(directive) {
             $scope.directives.push(directive);
         }
